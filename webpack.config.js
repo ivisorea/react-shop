c35da85d4b34952fc3385c28e263ded8bf1628db
Ivis Orea <ivisorea@gmail.com>
2022-03-03 09:36:30 +0100

    ADD Global Styles File

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');    
 
 module.exports = {
     entry: './src/index.js',
@@ -28,15 +29,33 @@ module.exports = {
                     },
                 ],
             },
-            
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
         ],
-    },
+        },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html',
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3000,
+        open: true,
+    },
 };
 
 
